6bdf17f5efd6edb5022d296b73aa045b5336c2fb
Annie Huang <annie.hn@gmail.com>
2022-01-30 17:40:17 +1100

    Get the state data throug useSelector and display it in the UI.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getLorem } from './store/features/lorem/loremSlice';
 
 function App() {
   const dispatch = useDispatch();
 
+  // find the slice in store.js
+  const lorem = useSelector((state) => state.lorem);
+
   useEffect(() => {
     dispatch(getLorem());
   }, []);
 
-  return <div></div>;
+  return <div> {JSON.stringify(lorem)}</div>;
 }
 
 export default App;
