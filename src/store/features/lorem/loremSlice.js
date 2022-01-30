e192dc02feddce75cb113e84b1efb3f791ee9cc2
Annie Huang <annie.hn@gmail.com>
2022-01-30 16:47:06 +1100

    Create getLorem AsyncThunk

--- src/store/features/lorem/loremSlice.js
+++ src/store/features/lorem/loremSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const getLorem = createAsyncThunk(
+  'lorem/getData',
+  (arg, { rejectWithValue }) => {
+    try {
+      const { data } = axios.get('https://baconipsum.com/api/?type=tst');
+      return data;
+    } catch (error) {
+      rejectWithValue(error.respond.data);
+    }
+  }
+);
 
 const loremSlice = createSlice({
   name: 'lorem',
